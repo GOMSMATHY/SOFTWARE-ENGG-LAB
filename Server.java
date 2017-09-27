170b7908a448a35befd2a64d7557ffc530960066
GOMSMATHY <32321699+GOMSMATHY@users.noreply.github.com>
2017-09-27 12:20:02 +0530

    server added

--- Server.java
+++ Server.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-public class Client{
+public class Server{
 
 	public static void main(String[] args)throws Throwable{
 		Socket soc = new Socket("localhost",3000);
